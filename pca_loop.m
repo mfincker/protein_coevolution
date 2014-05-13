773ebd1f8fb6f367939c0b046a4089e261399890
Leah Sibener <leahsibener@dn52e684.sunet>
2014-05-13 15:29:37 -0700

    working on loop

--- pca_loop.m
+++ pca_loop.m
@@ -1,12 +1,14 @@
-function [ PCA_loop_results ] = pca_loop( sector_database, showplots)
+function [ PCA_loop_results ] = pca_loop( sectorDatabase, showplots)
 %This function will be able to perform PCA over all sectors in the database
     %pca_loop will return the pca results, and plot and compare differnet
     %pca results for all sectors. 
+
 % to keep track of the  lambdas for each sector.
 PCAresults = zeros(numel(sectorDatabase), 3) 
+size(PCAresults)
 
 for i = 1:numel(sectorDatabase)
-    PCAresults( i, :) = sector_pca( sectorDatabase{i} )
+    PCAresults( i, :) = sector_pca( sectorDatabase{1, i}.Coordinates, 0 )
 end
 
 % visualize the data in a meaningful way.
