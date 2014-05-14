e9a727871e23cfab0cab25a012c776de94a09d0c
Leah Sibener <leahsibener@rescomp-13-261141.stanford.edu>
2014-05-14 00:03:53 -0700

    Loop will now give all eigenvectors!
    Still need to amend for overlapping plotting.

--- pca_loop.m
+++ pca_loop.m
@@ -1,15 +1,21 @@
-function [ PCA_loop_results ] = pca_loop( sectorDatabase, showplots)
+function [ eigenvector_sector_database ] = pca_loop( sectorDatabase, showplots)
 %This function will be able to perform PCA over all sectors in the database
     %pca_loop will return the pca results, and plot and compare differnet
     %pca results for all sectors. 
+    
 
 % to keep track of the  lambdas for each sector.
-PCAresults = zeros(numel(sectorDatabase), 3) 
-size(PCAresults)
 
-for i = 1:numel(sectorDatabase)
-    PCAresults( i, :) = sector_pca( sectorDatabase{1, i}.Coordinates, 0 )
-end
+eigenvector_sector_database = cell(1,length(sectorDatabase));
+% get eigenvectors from sector_PCA on all sectors
+
+    for i = 1:numel(sectorDatabase)
+    [sect_eigvectors] = sector_pca(sectorDatabase{1,i}, showplots )
+        for i = 1:length(sectorDatabase)
+            eigenvector_sector_database{:,i} = sect_eigvectors
+            
+        end
+    end
 
 % visualize the data in a meaningful way.
 
