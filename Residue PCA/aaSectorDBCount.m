a69f0ec5f51e8179105d8ccfd4996a4b056f4447
Maeva Fincker <mfincker@stanford.edu>
2014-05-13 17:34:05 -0700

    aaSectorDBCount function
    For PCA on residue: count the occurrence of each aa in each sector in
    the sectorDB given in argument.

---
+++ Residue PCA/aaSectorDBCount.m
@@ -0,0 +1,16 @@
+function [ aaMatrixCount] = aaSectorDBCount ( sectorDatabase )
+%AASECTORDBCOUNT returns a 20 x n matrix where n is the number
+%of sectors in sectorDatabase. Each row corresponds to an amino
+%acid - the order of the aa is the same as the order of aa
+%from the aacount() output structure. The cell (i,j) is populated
+%with the number of occurence of amino acid (i) in sector (j).
+	aaMatrixCount = zeros(20, numel(sectorDatabase));
+	for i = 1:numel(sectorDatabase)
+		seq = sectorDatabase{i}.Sequence;
+		count = aacount(seq);
+		count = struct2cell(count);
+		count = cell2mat(count);
+		aaMatrixCount(:,i) = count;
+	end
+
+end
