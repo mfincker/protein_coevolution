5173aae5a3f863a7083390b5eef8e5453b73231e
Alexandre Colavin <acolavin@stanford.edu>
2014-05-17 15:13:24 -0700

    Added wrapper that returns clusters for accession
    And fixed some bugs…

--- MISC Wrapper/miscwrapper.m
+++ MISC Wrapper/miscwrapper.m
@@ -1,4 +1,4 @@
-function [refclusts,varargout] = miscwrapper(msa,refseq,refind)
+function [combrefclusts,varargout] = miscwrapper(msa,refseq,refind)
 %MISCWRAPPER    wrapper for Mutual Information Sequence Correlation.
 %   sectors = MISCWRAPPER(msa,refseqind) for integer MSA (numseq x numpos)
 %   and reference sequence residues refseq and position numbers refind will
@@ -39,7 +39,7 @@ removegaps = mode(double(msa),1)==0;
 % Prune away conserved positions
 removecons = zeros(1,numpos);
 for curpos = 1:numpos
-    if mode(double(msa(:,curpos)))==0
+    if nnz(msa(:,curpos)==msa(1,curpos))==length(msa(:,curpos))
         removecons(curpos) = 1;
     end
 end
@@ -52,7 +52,7 @@ n = size(msap,2);
 Cp = reshape(1-misc_pc(msap),[n,n]);
 
 % conduct bootstrapping
-numboot = round(.01*numseq);
+numboot = round(.05*numseq);
 allCp = zeros(n,n,numboot);
 for l = 1:numboot
     allCp(:,:,l) = reshape(1-misc_pc(msap(randi(numseq,1,numseq),:)),[n,n]);
@@ -67,17 +67,21 @@ Cpfinal = median(allCp,3);
 % and remove clusters with zero residues
 % or with average or below-average scores.
 refclusts = {};
+rawrefclusts = {};
 avemisc = median(Cpfinal(:));
 stdmisc = std(Cpfinal(:));
+threshmisc = .3*stdmisc;
 reorder = 0;%zeros(1,n);
 pos = 1;
 for l = 1:length(clusts)
     % determine if it's a good cluster, and save it
-    if (median(Cpfinal(clusts{l},clusts{l})) - avemisc) > .2*stdmisc
+    curkeep = Cpfinal(clusts{l},clusts{l});
+    if median(curkeep(:)) - avemisc > threshmisc
         curclust = atsp(clusts{l});
         curclust(~curclust) = [];
-        if length(curclust)>4 && length(curclust) < 100
+        if length(curclust)>=3
             refclusts{end+1} = curclust;
+            rawrefclusts{end+1} = clusts{l};
             % reorder to show clusters in matrix
 %             if ~isempty(clusts{l})
                 reorder(pos:(pos+length(clusts{l})-1)) = clusts{l};
@@ -88,23 +92,47 @@ for l = 1:length(clusts)
 
 end
 
+% Now we combine those that have high co-coevolution
+taken = zeros(1,length(refclusts));
+combrefclusts = {};
+for l = 1:length(refclusts)
+    if ~taken(l)
+        taken(l) = 1;
+        combrefclusts{end+1} = refclusts{l};
+        for g = (l+1):length(refclusts)
+            if ~taken(g)
+                curkeep = Cpfinal(rawrefclusts{l},rawrefclusts{g});
+                if (median(curkeep(:)) - avemisc) > threshmisc
+                    taken(g) = 1;
+                    combrefclusts{end} = [combrefclusts{end} refclusts{g}];
+                end
+            end
+        end
+    end
+end
+
+
 % assign second argument if it was asked for.
 if nargout == 2
     misc = struct([]);
-    misc(1).rawmisc = Cp;
-    misc(1).rawbootmisc = Cpfinal;
+    misc(1).rawmisc = Cp; % co-evolution matrix on all data
+    misc(1).rawbootmisc = Cpfinal; % boot-strapped co-evolution matrix
     Cpfinal = Cpfinal(reorder,:);
     Cpfinal = Cpfinal(:,reorder);
     Cp = Cp(reorder,:);
     Cp = Cp(:,reorder);
-    misc(1).bootmisc = Cpfinal;
-    misc(1).misc = Cp;
-    misc(1).rawats = atsp;
-    misc(1).ats = atsp(reorder);
-    misc(1).reorder = reorder;
-    misc(1).ignored_residues = removed_residues(removed_residues~=0);
-    misc(1).included_residues = atsp(atsp~=0);
+    misc(1).bootmisc = Cpfinal;  % cluster-reordered bootstrapped matrix
+    misc(1).misc = Cp; % cluster-reordered matrix
+    misc(1).rawats = atsp; % index-to-residue index mapping
+    misc(1).ats = atsp(reorder); % reordered index-to-residue index mappin
+    misc(1).reorder = reorder; % how things were reoridered.
+    misc(1).ignored_residues = removed_residues(removed_residues~=0); % ignored protein residues numbers
+    misc(1).included_residues = atsp(atsp~=0); % included protein residue numbers (not necessarily in a cluster)
+    misc(1).rawclusts = refclusts; % clusters recombined if they have cross-coevolution
+    misc(1).msap = msap; % pruned msa
+    misc(1).msa = msa; % input msa
     varargout{1} = misc;
+    
 end
 
 end
