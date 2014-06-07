515e17e076c7356ed67ca2d41908d20ffaa0dc13
Maeva Fincker <mfincker@stanford.edu>
2014-06-06 17:20:04 -0700

    Clean up

--- Figures/READ_ME.txt
+++
@@ -1,3 +0,0 @@
-Folder to store the figures for the final paper
-
-If your figure comes with a separate file for the legend, make sure the legend file is called <figure file name>_legend.txt
---
+++ Scripts/pca_loop.m
@@ -0,0 +1,37 @@
+function [ eigenvector_sector_database, eigenvalue_sector_database] = pca_loop( sectorDatabase, showplots)
+%This function will be able to perform PCA over all sectors in the database
+    %pca_loop will return the pca results, and plot and compare differnet
+    %pca results for all sectors. 
+    
+
+<<<<<<< HEAD
+% to keep track of the  lambdas for each sector.
+
+eigenvector_sector_database = cell(1,length(sectorDatabase))
+eigenvalue_sector_database = cell(1, length(sectorDatabase));
+% get eigenvectors from sector_PCA on all sectors
+
+    for i = 1:numel(sectorDatabase)
+    [sect_eigvectors, lambda_coordinates] = sector_pca(sectorDatabase{1,i}, showplots )
+        for i = 1:length(sectorDatabase)
+            eigenvector_sector_database{:,i} = sect_eigvectors
+            eigenvalue_sector_database{:,i} = lambda_coordinates
+            
+        end
+    end
+=======
+	% to keep track of the  lambdas for each sector.
+	PCAresults = zeros(3, numel(sectorDatabase)) ;
+	size(PCAresults)
+
+	for i = 1:numel(sectorDatabase)
+	    PCAresults( i, :) = sector_pca( sectorDatabase{i}.Coordinates, 0 )
+	end
+
+	% visualize the data in a meaningful way.
+>>>>>>> FETCH_HEAD
+
+% visualize the data in a meaningful way.
+%eigenvector_sector_database_matrix = cell2mat(eigenvector_sector_database)
+end
+
