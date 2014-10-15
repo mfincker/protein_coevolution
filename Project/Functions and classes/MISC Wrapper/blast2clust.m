adf3fe4b640bbf90284b1cfa2f6bc1fd4ca33a31
Maeva Fincker <mfincker@stanford.edu>
2014-10-15 15:02:38 -0700

    Removed MISC wrapper

--- Project/Functions and classes/MISC Wrapper/blast2clust.m
+++
@@ -1,9 +0,0 @@
-function [clusters,extra] = blast2clust(seqacc) 
-
-[msa,seedseq,seednum] = blast2msa(seqacc,500,1);
-% BRAD PRUNING FUNCTION GOES HERE
-% e.g. msa2 = phyloprunemsa(msa,cutoff)
-msa2 = msa; % remove this line when Brad adds function
-[clusters,extra] = miscwrapper(msa,seedseq,seednum);
-
-end
